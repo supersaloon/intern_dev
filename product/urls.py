6c1fccca0f8b413c041ea5fa90f8aafffa4921e0
supersaloon <supersaloon@gmail.com>
2021-01-25 16:42:28 +0900

    category_list GET 구현

--- product/urls.py
+++ product/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 
-from product.views.drink_views      import DrinkView
-from product.views.drink_list_views import DrinkListView
+from product.views.drink_views         import DrinkView
+from product.views.drink_list_views    import DrinkListView
+from product.views.category_list_views import CategoryListView
+
 
 
 urlpatterns = [
     path('/drink', DrinkView.as_view()),
     path('/drink_list', DrinkListView.as_view()),
-    path('/drink/<int:product_id>', DrinkView.as_view())
+    path('/drink/<int:product_id>', DrinkView.as_view()),
+
+    path('/category_list', CategoryListView.as_view()),
 ]
---
+++ product/views/category_list_views.py
@@ -0,0 +1,28 @@
+from django.http  import JsonResponse
+from django.views import View
+from django.db    import transaction
+
+from product.models import ProductCategory, DrinkCategory
+
+
+class CategoryListView(View):
+    # @signin_decorator
+    @transaction.atomic
+    def get(self, request):
+
+        try:
+            category_list_data = {
+                'product_category': [{
+                    'id'              : product_category.id,
+                    'product_category': product_category.name,
+                }for product_category in ProductCategory.objects.all()],
+                'drink_category': [{
+                    'id'            : drink_category.id,
+                    'drink_category': drink_category.name,
+                } for drink_category in DrinkCategory.objects.all()],
+            }
+
+            return JsonResponse({'MESSAGE': 'SUCCESS', "category_list_data": category_list_data}, status=201)
+
+        except KeyError as e:
+            return JsonResponse({"MESSAGE": "KEY_ERROR => " + e.args[0]}, status=400)
