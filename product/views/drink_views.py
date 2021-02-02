a6ef8763f69fe2b114e2d63045becd8cbfafb6ec
supersaloon <supersaloon@gmail.com>
2021-02-02 16:20:43 +0900

    drink_detail_volume -> drink_option 으로 고도화

--- product/views/drink_views.py
+++ product/views/drink_views.py
@@ -201,7 +201,7 @@ class DrinkView(View):
         try:
             product                 = Product.objects.get(id=product_id)
             drink_detail            = (DrinkDetail.objects
-                                         .prefetch_related("drinkdetailvolume_set__volume")
+                                         .prefetch_related("drinkoption_set__volume")
                                          .get(product_id=product.id))
             industrial_product_info = product.industrialproductinfo_set.get()
             taste_matrix            = drink_detail.tastematrix_set.get()
@@ -281,11 +281,12 @@ class DrinkView(View):
                 }for base_material in drink_detail.drink_detail_base_material.all()],
 
 
-                "volume_and_price"              : [{
-                                                        "id"    : volume.volume.id,
-                                                        "volume": volume.volume.name,
-                                                        "price" : volume.price,
-                } for volume in drink_detail.drinkdetailvolume_set.all()]
+                "drink_option"                  : [{
+                                                        "id"    : drink_option.volume.id,
+                                                        "volume": drink_option.volume.name,
+                                                        "price" : drink_option.price,
+                                                        "amount": drink_option.amount,
+                } for drink_option in drink_detail.drinkoption_set.all()]
             }
 
             return JsonResponse({'MESSAGE': 'SUCCESS', 'drink_data': drink_data}, status=200)
@@ -302,10 +303,10 @@ class DrinkView(View):
         try:
             data = json.loads(request.body)
 
-            drink_category, flag = DrinkCategory.objects.get_or_create(name=data['drink_category'])
+            drink_category, flag    = DrinkCategory.objects.get_or_create(name=data['drink_category'])
             product                 = Product.objects.get(id=product_id)
             drink_detail            = (DrinkDetail.objects
-                                         .prefetch_related("drinkdetailvolume_set__volume")
+                                         .prefetch_related("drinkoption_set__volume")
                                          .get(product_id=product.id))
             industrial_product_info = product.industrialproductinfo_set.get()
             taste_matrix            = drink_detail.tastematrix_set.get()
@@ -408,36 +409,43 @@ class DrinkView(View):
 
 
             # volume and price
-            for volume_and_price in data['volume_and_price']:
+            for drink_option in data['drink_option']:
+                '''
+                "drink_option":
+                [{"id": "32", "volume": "500ml" , "price": "5555" , "status": "add"},
+                {"id" : ""  , "volume": "800ml" , "price": "9000" , "status": "delete"},
+                {"id" : "40", "volume": "1200ml", "price": "12010", "status": "normal"}],
+                '''
                 # 사용자가 추가한 용량을 추가
-                if volume_and_price['status'] == "add":
-                    new_volume, flag = Volume.objects.get_or_create(name=volume_and_price['volume'])
-                    DrinkDetailVolume.objects.create(
+                if drink_option['status'] == "add":
+                    new_volume, flag = Volume.objects.get_or_create(name=drink_option['volume'])
+                    DrinkOption.objects.create(
                         drink_detail = drink_detail,
                         volume       = new_volume,
-                        price        = volume_and_price['price']
+                        price        = drink_option['price'],
+                        amount       = drink_option['amount'],
                     )
                     print(f"신규 용량: {new_volume.name} 추가 완료")
 
-                # 사용자가 삭제한 용량을 삭제한 경우
-                elif volume_and_price['status'] == "delete":
-                    volume = Volume.objects.get(id=volume_and_price['id'], name=volume_and_price['volume'])
-                    drink_detail_volume_relation = DrinkDetailVolume.objects.get(volume=volume, drink_detail=drink_detail)
-                    drink_detail_volume_relation.delete()
+                # 사용자가 삭제한 용량을 삭제 처리
+                elif drink_option['status'] == "delete":
+                    volume = Volume.objects.get(id=drink_option['id'], name=drink_option['volume'])
+                    drink_option_relation = DrinkOption.objects.get(volume=volume, drink_detail=drink_detail)
+                    drink_option_relation.delete()
                     print(f"용량: {volume.name} 삭제 완료")
 
-                    # 음료 상세와 관계가 제거된 용량의 참조횟수가 0이면 용량 삭제
-                    volume_ref_count = DrinkDetailVolume.objects.filter(volume=volume).count()
+                    # volume의 참조횟수가 0이면 Volume 자체를 삭제 처리
+                    volume_ref_count = DrinkOption.objects.filter(volume=volume).count()
                     print(f'>>>>> ref count of {volume} is {volume_ref_count}')
                     if volume_ref_count == 0:
                         volume.delete()
 
-                elif volume_and_price['status'] == "normal":
+                elif drink_option['status'] == "normal":
                     # 신규 또는 삭제가 아닌 경우 -> price 덮어쓰기
-                    volume = Volume.objects.get(id=volume_and_price['id'], name=volume_and_price['volume'])
-                    drink_detail_volume_relation = DrinkDetailVolume.objects.get(volume=volume, drink_detail=drink_detail)
-                    drink_detail_volume_relation.price = volume_and_price['price']
-                    drink_detail_volume_relation.save()
+                    volume = Volume.objects.get(id=drink_option['id'], name=drink_option['volume'])
+                    drink_option_relation = DrinkOption.objects.get(volume=volume, drink_detail=drink_detail)
+                    drink_option_relation.price = drink_option['price']
+                    drink_option_relation.save()
 
 
             # industrial_product_infos 테이블
