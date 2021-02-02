5ae8a2514d3803911fbd40066e339991a3fb9a80
supersaloon <supersaloon@gmail.com>
2021-02-02 14:02:51 +0900

    이미지 업로드 url 수정

--- product/urls.py
+++ product/urls.py
@@ -17,8 +17,8 @@ urlpatterns = [
     path('/drink_list', DrinkListView.as_view()),
     path('/drink/<int:product_id>', DrinkView.as_view()),
 
-    path('/image', ProductImageView.as_view()),
-    path('/image/<int:product_image_id>', ProductImageView.as_view()),
+    path('/product_image', ProductImageView.as_view()),
+    path('/product_image/<int:product_image_id>', ProductImageView.as_view()),
 
     path('/label', LabelView.as_view()),
     path('/label/<int:label_id>', LabelView.as_view()),
