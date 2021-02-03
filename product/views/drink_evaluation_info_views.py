b54547571dc924f961c4561cf60a5b90d1f29f79
supersaloon <supersaloon@gmail.com>
2021-02-04 01:19:05 +0900

    자체평가 디버깅

--- product/views/drink_evaluation_info_views.py
+++ product/views/drink_evaluation_info_views.py
@@ -69,8 +69,8 @@ class DrinkEvaluationInfoView(View):
             return JsonResponse({"MESSAGE": "INTEGRITY_ERROR => " + e.args[0]}, status=400)
         except KeyError as e:
             return JsonResponse({"MESSAGE": "KEY_ERROR => " + e.args[0]}, status=400)
-        except Exception as e:
-            return JsonResponse({"MESSAGE": "Exception => " + str(e)}, status=400)
+        # except Exception as e:
+        #     return JsonResponse({"MESSAGE": "Exception => " + str(e)}, status=400)
 
 
     # def get(self, request, product_id):
